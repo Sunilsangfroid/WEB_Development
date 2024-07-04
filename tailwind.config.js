4a57427f33c34fa844927e1b9a74d5cc86e129ca
Sunilsangfroid <sunilsangfroid@gmail.com>
2024-07-04 07:00:19 +0530

    Completed with navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mullish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightBlue:"#2b84ea",
+        gray2:"#525a76",
+        greenLight:"#61cea6",
+        lightBlue300:"#4b94ed",
+        deepBlueHead:"#162f56",
+        grayText:"#818597",
+        grayBlue:"#344a6c",
+        lightGray:"#e2e2e2",
+        lightBlue500:"#ob72e7",
+      }
+    },
+  },
+  plugins: [],
+}
+
